aeb87b7dccaf2337839d5e782073b03571fb3348
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-11 03:49:31 +0000

    size tester

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+printf("Size of a char: %d bytes(s)\n", sizeof(c));
+printf("Size of a int: %d bytes(s)\n", sizeof(i));
+printf("Size of a long int: %d byte(s)\n", sizeof(li));
+printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+printf("SIze of a float: %d byte(s)\n", sizeof(f));
+return (0);
+}
