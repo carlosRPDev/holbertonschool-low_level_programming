b5631d92d36b27e867b8205892098da171b1255a
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-11 04:08:56 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,13 @@ int main(void)
 {
 char c;
 int i;
-long l;
-long long ll;
+long int li;
+long long int lli;
 float f;
-printf("Size of a char: %d bytes(s)\n", sizeof(c));
-printf("Size of an int: %d bytes(s)\n", sizeof(i));
-printf("Size of a long int: %d byte(s)\n", sizeof(l));
-printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long int: %d byte(s)\n", sizeof(li));
+printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
 printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
