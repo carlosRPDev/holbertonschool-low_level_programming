f5c70344fc786770c7ab0a10c141beb5dc38fd02
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-11 03:59:48 +0000

    Update space size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,13 +9,13 @@ int main(void)
 {
 char c;
 int i;
-long int li;
-long long int lli;
+long l;
+long long ll;
 float f;
 printf("Size of a char: %d bytes(s)\n", sizeof(c));
 printf("Size of an int: %d bytes(s)\n", sizeof(i));
-printf("Size of a long int: %d byte(s)\n", sizeof(li));
-printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+printf("Size of a long int: %d byte(s)\n", sizeof(l));
+printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
 printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
