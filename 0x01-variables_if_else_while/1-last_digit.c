fdbe48833cd615affd794c18de50416de7dba016
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-12 04:11:55 +0000

    Last digit solution error in sentence if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,14 +15,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	u = n % 10;
 
-	if (n > 5)
+	if (u > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, u);
 	}
-	else if (n == 0)
+	else if (u == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, u);
 	}
