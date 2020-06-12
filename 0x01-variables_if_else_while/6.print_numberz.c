b7e12a263246d703a91d04fc7ffd24f6b2593fff
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-12 22:38:27 +0000

    print number 1..9 with putchar

---
+++ 0x01-variables_if_else_while/6.print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = '0'; l <= '9'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
