c0d46f86daac8666a34bbc78f815c68b92be6e55
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-13 01:17:34 +0000

    print secuencia 0..1 and space and coma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 48; l <= 57; l++)
+	{
+		putchar(l);
+		if (l != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
