4ea219ad5a02c3f11bbba81336c147c6fe6b50d0
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-16 18:21:39 +0000

    value absolute function abs in c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _abs - print if n is positive or zero or negative
+ *
+ *@n: value at verific
+ *
+ * Return: 0 sucesfull
+ */
+
+int _abs(int n)
+{
+	n = abs(n);
+	return (n);
+}
