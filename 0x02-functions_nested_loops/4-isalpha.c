0dde5ee81b20bd1ce2152288738be4a839abdedd
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-16 16:22:35 +0000

    Verific if letter is lowercase or uppercase update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,12 +10,16 @@
 
 int _isalpha(int c)
 {
-	if (isalpha(c) == 0)
+	if ((c >= 97) && (c <= 122))
 	{
-		return (0);
+		return (1);
+	}
+	else if ((c >= 65) && (c <= 90))
+	{
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
