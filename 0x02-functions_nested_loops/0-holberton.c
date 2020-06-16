73bb4763dd01b25cf3cbd15f36931237af0eaebf
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-16 22:29:11 +0000

    Print word Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-	int i = 0;
-
-	char word[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
-
-	while (i <= 9)
-	{
-		_putchar(word[i]);
-		i++;
-	}
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
 	_putchar(10);
 	return (0);
 }
