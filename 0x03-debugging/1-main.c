42187b0e130e0bc51f53a7ccdbb994a87d950dac
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-17 01:58:30 +0000

    Comment infinite loop and betty update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,16 +10,16 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
-	i = 0;
 	/**
-	* don´t delete it!
+	* don't delete it!
+	* i = 0;
+	*
 	* while (i < 10)
 	* {
 	*	putchar(i);
 	* }
-	*
-	*printf("Infinite loop avoided! \\o/\n");
 	*/
+	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
