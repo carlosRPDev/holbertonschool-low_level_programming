35af4d57d178902d41dc5fb901b4531ae16bf5cf
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-17 02:15:01 +0000

    Comment infinite loop update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,15 +12,15 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	/**
-	* don't delete it!
-	*
-	* i = 0;
-	*
-	* while (i < 10)
-	* {
-	*	putchar(i);
-	* }
-	*/
+	 * don't delete it!
+	 *
+	 * i = 0;
+	 *
+	 * while (i < 10)
+	 * {
+	 *	putchar(i);
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
