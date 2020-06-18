4cadc07ad2400572144d784d1a3acf793466a823
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-18 22:12:57 +0000

    Update task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,42 +8,42 @@
  */
 void times_table(void)
 {
+	int r, d, l, c;
 
-	int l;
-	int c;
-	int r;
-	int d;
-
-	l = 0;
-	while (l < 10)
+	for (l = 0; l <= 9; l++)
 	{
-		c = 0;
-		while (c < 10)
+		for (c = 0; c <= 9; c++)
 		{
 			r = l * c;
-			if (r < 10)
+			if (r >= 10)
 			{
-				_putchar(' ');
+			_putchar(r / 10 + '0');
+			d = r % 10;
+			_putchar(d + '0');
 			}
 			else
 			{
-				d = r / 10;
-				_putchar('0' + d);
+				_putchar(r + '0');
 			}
-			d = r % 10;
-			_putchar('0' + d);
+
 			if (c == 9)
 			{
-				_putchar('\n');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
+				if (l * (c + 1) >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
 			}
-			c++;
 		}
 		_putchar('\n');
-		l++;
 	}
 }
