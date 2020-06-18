1e029ff20c42389f9a21b413bded96f09440d470
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-18 02:43:58 +0000

    Leap year

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -2,14 +2,13 @@
 #include "holberton.h"
 
 /**
- * main - takes a date and prints how many days are left in the year, taking
- * leap years into account
- * Return: 0
- */
+* main - takes a date and prints how many days are left in the year, taking
+* leap years into account
+* Return: 0
+*/
 
 int main(void)
 {
-
 	int month;
 	int day;
 	int year;
@@ -25,5 +24,5 @@ int main(void)
 	print_remaining_days(month, day, year);
 
 	return (0);
-
 }
+
