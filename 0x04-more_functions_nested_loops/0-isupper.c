de633baccec46f10a03e67f77b443548ba362989
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-18 23:37:27 +0000

    function isupper(int c)

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <ctype.h>
 /**
  *_isupper - chekc if char is upper
  *@c: value of char
@@ -8,12 +8,12 @@
 
 int _isupper(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (isupper(c))
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return(1);
+		return(0);
 	}
 }
