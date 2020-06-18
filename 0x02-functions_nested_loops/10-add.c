685af6a358d62c234e3fca4f8b1c5c7aefd8235a
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-18 22:19:14 +0000

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/* more headers goes there */
+/**
+*add-prototype function,
+*function print 1 if the char is a letter
+*@n1: number
+*@n2: number
+*Return: the return of finish file
+*/
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);
+}
