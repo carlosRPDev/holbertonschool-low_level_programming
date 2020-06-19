8c15e58ab1744b20d4015438acd4b42ca78e9033
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-19 00:13:28 +0000

    funtion multiples two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *mul(int a, int b) - chekc if char is upper
+ *@a: value of int
+ *@b: value of int
+ *Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+
+	return (res);
+}
