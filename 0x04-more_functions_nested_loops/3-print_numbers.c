da9b9afec1518f53648f31f73cf13ab8b7c18ffd
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-19 03:42:01 +0000

    Loops for print 1..9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - prints numer of 0 ...9
+ *@i: value of int
+ *
+ *Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
