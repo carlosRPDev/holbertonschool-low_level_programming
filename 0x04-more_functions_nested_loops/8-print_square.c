8b5283b80d57dcc0dc6557d283776c0a9cc2b845
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-20 16:43:08 +0000

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *print_square - print square
+ *@s: numbers of lines
+ *@size: return
+ *Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int s, l;
+
+	for (s = 0; s < size; s++)
+	{
+		for (l = 0; l < size; l++)
+		{
+			_putchar(35);
+		}
+		_putchar(10);
+	}
+	if (size <= 0)
+		_putchar(10);
+}
