3addcade046ded2037ddb4751b982bd9d700fa65
carlos Rodriguez <ucroxlive@gmail.com>
2020-06-24 02:47:49 +0000

    Readme, answer task zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - print the value pointer
+ *
+ * @n: assigned value of pointer
+ *
+ * Return: 0 if was executed sucesfully
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
