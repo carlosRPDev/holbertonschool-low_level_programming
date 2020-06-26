e97d8800a522049cea9f5c0932153e4ff85f2414
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-26 02:20:30 +0000

    Funtion print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rev_rev - print reverse a string
+ * @s: variable that contain a reserved string
+ */
+void print_rev(char *s)
+{
+	int a;
+	int b;
+	int c;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+
+	c = a;
+	for (b = c - 1; b >= 0; b--)
+	{
+		_putchar(s[b]);
+	}
+	_putchar('\n');
+}
