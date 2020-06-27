9a480a012265e59280fabd360a863922135594b3
Carlos Rodriguez <ucroxlive@gmail.com>
2020-06-27 02:21:11 +0000

    copy string of the pointer to pointer

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * *_strncpy - Concatenates two strings
+ *
+ * @dest: value of the pointer
+ * @src: value of the other pointer
+ * @n: n values
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+		for ( ; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
+	}
+	return (dest);
+}
